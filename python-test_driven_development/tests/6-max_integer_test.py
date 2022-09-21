60232b26f553516feb53cfed2eb500ae4c3a8406
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-09-21 20:42:57 -0300

    Add task 6

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Unittest for the max_integer module
+"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """TestCase for the max_integer function."""
+
+    def test_max_beg(self):
+        self.assertEqual(max_integer([6, 4, 2]), 6)
