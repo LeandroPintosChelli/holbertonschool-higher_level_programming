4a9fef121471c8224be0917556da8cd5c3a6b644
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-09-26 13:29:26 -0300

    Fix task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
     """Determines if obj is an instance of a class that
     inherited from a_class."""
 
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) != a_class
