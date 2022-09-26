06174e7e270b990cdc0e809f04d21d0559a5198d
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-09-26 13:28:14 -0300

    Add task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that returns True if the object is an instance of a class
+that inherited (directly or indirectly) from the specified class
+otherwise False"""
+
+
+def inherits_from(obj, a_class):
+    """Determines if obj is an instance of a class that
+    inherited from a_class."""
+
+    return isinstance(obj, a_class)
