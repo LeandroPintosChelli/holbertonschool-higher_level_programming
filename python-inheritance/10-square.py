b11bc18a0dbdf70e99f3f6c58721eb6d90e70667
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-09-26 17:00:27 -0300

    Fix task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Module 10-square.
-Creates a Square class.
-"""
+"""Creates a Square class."""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
@@ -9,17 +7,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square.
-    Private instance attribute size.
-    Public method area().
-    Inherits from Rectangle.
-    """
+    """Represents a square."""
 
     def __init__(self, size):
-        """Initializes a Square.
-        Args:
-            - size: size of the square
-        """
+        """Initializes a Square."""
 
         self.integer_validator("size", size)
         super().__init__(size, size)
