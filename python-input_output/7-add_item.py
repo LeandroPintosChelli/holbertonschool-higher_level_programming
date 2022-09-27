cc4285a0cf4bb212476c2721715cc88528ded574
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-09-27 15:31:19 -0300

    Fix task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" Module that adds all arguments to a Python list, and then
+"""Adds all arguments to a Python list, and then
 save them to a file
 """
+
+
 import sys
+
+
 import os.path
 
 
