91140d2453e75de8b7ef8a9575038a323a3455f4
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-09-27 13:01:27 -0300

    Add task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Creates an Object from a “JSON file”."""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an object from filename"""
+
+    with open(filename, 'r') as file:
+        return json.load(file)
