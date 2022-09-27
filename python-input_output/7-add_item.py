b85b4f8003250991fd54d55b49c80ff0e365fb62
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-09-27 15:20:05 -0300

    Fix task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,15 +7,15 @@ import sys
 import json
 
 
-save = __import__('5-save_to_json_file').save_to_json_file
-load_from = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == '__main__':
     load = []
-try:
-    load = load_from('add_item.json')
-except:
-    pass
-for i in range (1, len(sys.argv)):
-    load.append(sys.argv[i])
-save(load, 'add_item.json')
+    try:
+        load = load_from_json_file('add_item.json')
+    except:
+        pass
+    for i in range (1, len(sys.argv)):
+        load.append(sys.argv[i])
+    save_to_json_file(load, 'add_item.json')
