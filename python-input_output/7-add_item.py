20e6376ead0370329249811aaba3af508832b6a6
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-09-27 15:04:58 -0300

    Fix task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,6 +11,6 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 open("add_item.json", "a")
 try:
     load = load_from_json_file("add_item.json")
-except ValueError:
+except:
     load = []
 save_to_json_file(load + sys.argv[:], "add_item.json")
