0f0bd7d017d279e55570caff05dc988486a410f7
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-09-27 11:23:09 -0300

    Fix comments task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""Module 3-write_file.
-Writes in a text file.
-"""
+"""Writes in a text file."""
 
 
 def write_file(filename="", text=""):
-    """Writes text in filename.
-    Args:
-        - filename: name of the file
-        - text: string to write in the file
-    Returns: number of characters written
-    """
+    """Writes text in filename."""
 
     with open(filename, 'w+') as file:
         return file.write(text)
