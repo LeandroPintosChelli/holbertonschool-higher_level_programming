d3fe63843a5eda5e93ebaa81602b470cac12f6b7
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-10-01 13:26:31 -0300

    Add task 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Defines a Base class for other classes in the project."""
+
+import json
+import os
+import csv
+
+
+class Base:
+    """Class with:
+    Private class attribute: __nb_objects"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialization of a Base instance"""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
