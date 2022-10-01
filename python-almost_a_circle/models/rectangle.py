6f69c4ff312d07ae49a043f10a62886753345201
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-10-01 13:41:08 -0300

    Fix task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,10 +18,10 @@ class Rectangle(Base):
             - id: id
             """
 
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
