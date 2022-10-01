e892f5ae7a400181e4bb987a3ec7cc2250742e8e
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-10-01 15:06:01 -0300

    Fix task 9

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -118,16 +118,19 @@ class Rectangle(Base):
                 - 4th argument represents x attribute
                 - 5th argument represents y attribute
         """
-
-        self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
         else:
             for key, value in kwargs.items():
                 if key == "id":
