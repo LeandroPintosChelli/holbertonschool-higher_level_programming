a1aa36fef69dbf3c59135baf4aaa0502e9b56356
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-10-03 10:09:07 -0300

    Add task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module square.
+Create a Square class, inheriting from Rectangle.
+"""
+
+from models.base import Base
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """Class describing a square.
+    Inherits from Rectangle."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes a Square instance.
+        Args:
+            - __size: size
+            - __x: position
+            - __y: position
+            - id: id
+        """
+
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns a string representation of a Square instance."""
+
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.__width)
