93d01e3e33a25eb457c6ee99951e197b52f93780
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-11-08 10:19:23 -0300

    Add task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+list all table values (table 'states')
+parameters given to script: username, password, database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to database
+    database = MySQLdb.connect(user=argv[1],
+                               password=argv[2],
+                               database=argv[3],
+                               host="localhost",
+                               port=3306)
+
+    cursor = database.cursor()
+    cursor.execute("""SELECT cities.name
+                    FROM states
+                    INNER JOIN cities ON states.id = cities.states_id
+                    WHERE states.name LIKE %s
+                    ORDER BY cities.id ASC""", (argv[4]))
+    for row in cursor.fetchall():
+        print(row)
