132ef22b6bba0a9c0f3606482b0ae5166d9fe32d
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-11-08 13:09:52 -0300

    Add task 8 and fix task 7

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,5 +15,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
+    state = session.query(State).order_by(State.id).first()
+
+    if state is None:
+        print("Nothing")
+    else:
         print("{}: {}".format(state.id, state.name))
