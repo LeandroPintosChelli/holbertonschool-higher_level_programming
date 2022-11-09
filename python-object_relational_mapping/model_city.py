52db04416d5611dd2d668a9a5523b80fb46c1dc5
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-11-09 13:32:01 -0300

    Fix task 14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -15,6 +15,6 @@ class City(Base):
     Class City: instance of Base
     """
     __tablename__ = "cities"
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
