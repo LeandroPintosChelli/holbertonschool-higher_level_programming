c9e3b4e7a62c43a42b3d3db23e1bc8a47f896edb
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-12-06 01:45:52 +0000

    Add fix 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const lines of line) {
+  console.log(lines);
+}
