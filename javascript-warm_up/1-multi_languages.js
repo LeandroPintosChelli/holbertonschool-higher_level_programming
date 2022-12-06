6466a8b86f68ce315facc1ce892406aba98783cf
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-12-06 00:47:26 +0000

    Fix task 1

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
 
-const C = 'C is fun';
-const P = 'Python is cool';
-const J = 'JavaScript is amazing';
-console.log(C, '\n', P, '\n', J);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
